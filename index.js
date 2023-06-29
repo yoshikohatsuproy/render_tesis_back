27f81f05a91ccf2f02979426e69d06ae7e288377
Jonathan <yokun12342@gmail.com>
2023-06-29 09:55:44 -0500

    Index

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { config } from 'dotenv'
 import app from './app.js'
 import { PORT } from  './config.js'
 import {sequelize} from './database/db.js'
@@ -5,6 +6,7 @@ import {sequelize} from './database/db.js'
 async function main(){
     try{
         app.listen(PORT)
+        console.log('Config', config())
         console.log('Server running on', PORT)
         await sequelize.authenticate();
         console.log('Nos hemos conectado a la base de datos')
